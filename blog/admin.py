294a4fcb6128ba7158d88d25c781b12cd4d5b1b7
Valerius Gorbenko <valerius.gorbenko@adesso.de>
2026-05-06 11:23:52 +0000

    Finish Introduction and Django Admin

---
+++ blog/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from blog.models import Tag, Post
+
+admin.site.register(Tag)
+admin.site.register(Post, PostAdmin)
+
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
